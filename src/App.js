f2dd27a0f1fc9bc05be30458cd953c78d7ff173a
RaviAtal <raviatal44@gmail.com>
2023-03-15 14:35:55 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header";
 import { ChooseUs } from "./Components/ChooseUs";
 import { OurFeatured } from "./Components/OurFeatured";
 import Testimonials from "./Components/Testimonials";
+import { Footer } from "./Components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <ChooseUs />
       <OurFeatured />
       <Testimonials />
+      <Footer />
     </>
   );
 }
